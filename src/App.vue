ec97079de8a3f556e113116301c5b1b9e29c8afc
Kim-sung-woo <kim.sungwoo@nbt.com>
2018-04-16 13:42:27 +0900

    [UPDATE]

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <rwv-header></rwv-header>
     <router-view></router-view>
-    <rwv-footer></rwv-footer>
   </div>
 </template>
 
 <script>
-import RwvHeader from '@/components/TheHeader'
-import RwvFooter from '@/components/TheFooter'
 
 export default {
   name: 'App',
   components: {
-    RwvHeader,
-    RwvFooter
   }
 }
 </script>
